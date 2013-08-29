5e87b385d07de383fee886132c50c206a516d9db
Leif Gensert <leif@propertybase.com>
2013-08-29 23:30:10 +0200

    increase code coverage

--- spec/lib/executrix_spec.rb
+++ spec/lib/executrix_spec.rb
@@ -60,4 +60,45 @@ describe Executrix::Api do
       s.query(sobject_input, query_input)
     end
   end
+
+  context 'file upload' do
+    describe '#insert' do
+      attachment_data = {
+        'ParentId' => '00Kk0001908kqkDEAQ',
+        'Name' => 'attachment.pdf',
+        'Body' => File.new('.')
+      }
+
+      {
+        upsert: [nil,[attachment_data.dup], 'upsert_id'],
+        update: [nil,[attachment_data.dup]],
+        insert: [nil,[attachment_data.dup]],
+        delete: [nil,[attachment_data.dup]],
+      }.each do |method_name, values|
+        describe "##{method_name}" do
+          it 'should delegate to #start_job' do
+            Executrix::Connection
+              .should_receive(:connect)
+              .and_return(empty_connection)
+            s = described_class.new(nil, nil)
+            s.should_receive(:start_job)
+              .with(method_name.to_s, *values)
+            s.send(method_name, *values)
+          end
+
+          it 'should trigger correct workflow' do
+            Executrix::Connection
+              .should_receive(:connect)
+              .and_return(empty_connection)
+            s = described_class.new(nil, nil)
+            empty_connection.should_receive(:create_job).ordered
+            empty_connection.should_receive(:add_file_upload_batch).ordered
+            empty_connection.should_receive(:close_job).ordered
+            res = s.send(method_name, *values)
+            expect(res).to be_a(Executrix::Batch)
+          end
+        end
+      end
+    end
+  end
 end
